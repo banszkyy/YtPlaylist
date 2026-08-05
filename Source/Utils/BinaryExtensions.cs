44a8697d412a3bfc2046bde172bcf71aa1ab7b51
banszkyy <bacten2004@gmail.com>
2026-08-05 11:06:10 +0200

    meow

--- Source/Utils/BinaryExtensions.cs
+++ Source/Utils/BinaryExtensions.cs
@@ -2,49 +2,55 @@ namespace YtPlaylist;
 
 static class BinaryExtensions
 {
-    public static void Write<T>(this BinaryWriter writer, T? value, Action<T> callback) where T : struct
+    extension(BinaryWriter writer)
     {
-        writer.Write(value.HasValue);
-        if (value.HasValue)
+        public void Write<T>(T? value, Action<T> callback) where T : struct
         {
-            callback(value.Value);
+            writer.Write(value.HasValue);
+            if (value.HasValue)
+            {
+                callback(value.Value);
+            }
+        }
+
+        public void Write(DateTimeOffset value)
+        {
+            writer.Write(value.Ticks);
+            writer.Write(value.Offset);
+        }
+        public void Write(TimeSpan value) => writer.Write(value.Ticks);
+        public void Write(int? value) => writer.Write(value, writer.Write);
+        public void WriteList<T>(IReadOnlyCollection<T> values, Action<T> callback)
+        {
+            writer.Write(values.Count);
+            foreach (T item in values)
+            {
+                callback(item);
+            }
         }
     }
 
-    public static void Write(this BinaryWriter writer, DateTimeOffset value)
+    extension(BinaryReader reader)
     {
-        writer.Write(value.Ticks);
-        writer.Write(value.Offset);
-    }
-    public static void Write(this BinaryWriter writer, TimeSpan value) => writer.Write(value.Ticks);
-    public static void Write(this BinaryWriter writer, int? value) => writer.Write(value, writer.Write);
-    public static void WriteList<T>(this BinaryWriter writer, IReadOnlyCollection<T> values, Action<T> callback)
-    {
-        writer.Write(values.Count);
-        foreach (T item in values)
+        public T? ReadNullable<T>(Func<T> callback)
         {
-            callback(item);
+            bool hasValue = reader.ReadBoolean();
+            return hasValue ? callback() : default;
         }
-    }
 
-    public static T? ReadNullable<T>(this BinaryReader reader, Func<T> callback)
-    {
-        bool hasValue = reader.ReadBoolean();
-        return hasValue ? callback() : default;
-    }
-
-    public static int? ReadNullableInt32(this BinaryReader reader) => ReadNullable(reader, reader.ReadInt32);
-    public static DateTimeOffset ReadDateTimeOffset(this BinaryReader reader) => new(reader.ReadInt64(), reader.ReadTimeSpan());
-    public static TimeSpan ReadTimeSpan(this BinaryReader reader) => new(reader.ReadInt64());
-    public static List<T> ReadList<T>(this BinaryReader reader, Func<T> callback)
-    {
-        int count = reader.ReadInt32();
-        if (count < 0) throw new FormatException();
-        List<T> res = [];
-        for (int i = 0; i < count; i++)
+        public int? ReadNullableInt32() => ReadNullable(reader, reader.ReadInt32);
+        public DateTimeOffset ReadDateTimeOffset() => new(reader.ReadInt64(), reader.ReadTimeSpan());
+        public TimeSpan ReadTimeSpan() => new(reader.ReadInt64());
+        public List<T> ReadList<T>(Func<T> callback)
         {
-            res.Add(callback());
+            int count = reader.ReadInt32();
+            if (count < 0) throw new FormatException();
+            List<T> res = [];
+            for (int i = 0; i < count; i++)
+            {
+                res.Add(callback());
+            }
+            return res;
         }
-        return res;
     }
 }
